5660bde016ddee434cfbb6f54dcfeb22816a3b44
Gerardo Perez <gerardo.perez@usach.cl>
2025-06-16 19:07:27 -0400

    Added Headers - Added all the roles header that were missing

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,11 @@
   <v-app>
     <HeaderPatient v-if="rol === 'paciente'" />
     <HeaderMedico v-else-if="rol === 'medico'" />
-    <HeaderQuimico v-else-if="rol === 'quimico'" />
+    <HeaderQuimico v-else-if="rol === 'quimico_farmaceutico'" />
     <HeaderNurse v-else-if="rol === 'enfermero'" />
+    <HeaderTechnicalSupport v-else-if="rol === 'soporte_tecnico'" />
+    <HeaderSecretary v-else-if="rol === 'secretario'" />
+    <HeaderGerente v-else-if="rol === 'gerente'" />
     <HeaderUnlogged v-else />
     <v-main>
       <router-view />
@@ -20,9 +23,12 @@ import { ref, onMounted } from 'vue'
 import HeaderPatient from '@/components/header/HeaderPatient.vue'
 import HeaderMedico from '@/components/header/HeaderMedico.vue'
 import HeaderUnlogged from '@/components/header/HeaderUnlogged.vue'
+import HeaderTechnicalSupport from '@/components/header/HeaderTechnicalSupport.vue'
 import Footer from '@/components/Footer/Footer.vue'
 import HeaderQuimico from '@/components/header/HeaderQuimico.vue'
 import HeaderNurse from '@/components/header/HeaderNurse.vue'
+import HeaderSecretary from '@/components/header/HeaderSecretary.vue'
+import HeaderGerente from '@/components/header/HeaderGerente.vue'
 
 const rol = ref(null)
 onMounted(() => {
---
+++ src/components/header/HeaderGerente.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-app-bar color="#C73636" app fixed>
+        <v-btn to="/" icon class="ml-4 md-0">
+            <v-img src="/images/clinica_umbrella.png"
+                class="justify-center"
+                width="50"
+                height="50"
+                cover
+            ></v-img>
+        </v-btn>
+
+        <v-app-bar-title style="font-family:'Comic Sans'">
+            Clínica Umbrella
+        </v-app-bar-title>
+        <v-btn @click="cerrarSesion">Cerrar Sesión</v-btn>
+        <v-btn >Generar reporte</v-btn>
+        
+        <!-- Solo para médicos -->
+        <v-btn color="white" variant="flat" class="text-primary" to="/Agenda">
+            Agendar horas medicos</v-btn>
+    </v-app-bar>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+function cerrarSesion() {
+  // Elimina los datos del localStorage
+    localStorage.clear()
+
+  // Redirige a la pagina de inicio
+    window.location.href = '/'
+}
+</script>
